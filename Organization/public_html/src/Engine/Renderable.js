64f8375f7d715502940d893a1bdb7d2f41dad24a
ebergstein <ebergstein@sbcglobal.net>
2017-11-01 19:41:55 -0700

    Started Chapter 3.

---
+++ Organization/public_html/src/Engine/Renderable.js
@@ -0,0 +1,18 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+function Renderable(shader){
+    this.mShader = shader;
+    this.mColor = [1,1,1,1];
+}
+
+Renderable.prototype.draw = function(){
+    var gl = gEngine.Core.getGL();
+    this.mShader.activateShader(this.mColor);
+    gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
+};
+
+Renderable.prototype.setColor = function(color){ this.mColor = color; };
+Renderable.prototype.getColor = function(){ return this.mColor; };
--- Organization/public_html/src/MyGame/MyGame.js
+++ Organization/public_html/src/MyGame/MyGame.js
@@ -5,11 +5,13 @@
  */
 
 function MyGame(htmlCanvasID){
-    this.mShader = null;
     gEngine.Core.initializeWebGL(htmlCanvasID);
-    this.mShader = new SimpleShader("src/GLSLShaders/SimpleVS.glsl", "src/GLSLShaders/SimpleFS.glsl");
+    this.mConstColorShader = new SimpleShader("src/GLSLShaders/SimpleVS.glsl", "src/GLSLShaders/SimpleFS.glsl");
+    this.mWhiteSq = new Renderable(this.mConstColorShader);
+    this.mWhiteSq.setColor([1,1,1,1]);
+    this.mRedSq = new Renderable(this.mConstColorShader);
+    this.mRedSq.setColor([1,0,0,1]);
     gEngine.Core.clearCanvas([0, 0.8, 0, 1]);
-    this.mShader.activateShader([0, 0, 1, 1]);
-    var gl = gEngine.Core.getGL();
-    gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
+    this.mWhiteSq.draw();
+    this.mRedSq.draw();
 }
