c38d5ce1ecb640d268ca3f5f545440dd05c22b47
ebergstein <ebergstein@sbcglobal.net>
2017-11-07 16:52:58 -0800

    Completed Chapter 3

--- Organization/public_html/src/MyGame/MyGame.js
+++ Organization/public_html/src/MyGame/MyGame.js
@@ -6,7 +6,7 @@
 
 function MyGame(htmlCanvasID){
     gEngine.Core.initializeWebGL(htmlCanvasID);
-    var gl = gEngine.Core.getGL();
+    this.mCamera = new Camera(vec2.fromValues(20, 60), 20, [20, 40, 600, 300]);
     this.mConstColorShader = new SimpleShader("src/GLSLShaders/SimpleVS.glsl", "src/GLSLShaders/SimpleFS.glsl");
     this.mBlueSq = new Renderable(this.mConstColorShader);
     this.mBlueSq.setColor([0.25,0.25,0.95,1]);
@@ -21,17 +21,8 @@ function MyGame(htmlCanvasID){
     this.mBLSq = new Renderable(this.mConstColorShader);
     this.mBLSq.setColor([0.1,0.1,0.1,1]);
     gEngine.Core.clearCanvas([0.9, 0.9, 0.9, 1]);
-    gl.viewport(20, 40, 600, 300);
-    gl.scissor(20, 40, 600, 300);
-    gl.enable(gl.SCISSOR_TEST);
-    gEngine.Core.clearCanvas([0.8, 0.8, 0.8, 1.0]);
-    gl.disable(gl.SCISSOR_TEST);
-    var viewMatrix = mat4.create();
-    var projMatrix = mat4.create();
-    mat4.lookAt(viewMatrix, [20, 60, 10], [20, 60, 0], [0, 1, 0]);
-    mat4.ortho(projMatrix, -10, 10, -5, 5, 0, 1000);
-    var vpMatrix = mat4.create();
-    mat4.multiply(vpMatrix, projMatrix, viewMatrix);
+    this.mCamera.setupViewProjection();
+    var vpMatrix = this.mCamera.getVPMatrix();
     this.mBlueSq.getXForm().setPosition(20, 60);
     this.mBlueSq.getXForm().setRotationInRad(0.2);
     this.mBlueSq.getXForm().setSize(5, 5);
