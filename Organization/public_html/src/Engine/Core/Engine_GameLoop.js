dbef0e37dc58eadd61ce0900f1cb65a471689ffd
ebergstein <ebergstein@sbcglobal.net>
2017-11-08 19:33:38 -0800

    Implemented Movement.

---
+++ Organization/public_html/src/Engine/Core/Engine_GameLoop.js
@@ -0,0 +1,44 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+var gEngine = gEngine || {};
+
+gEngine.GameLoop = (function(){
+    var kFPS = 60;
+    var kMPF = 1000/kFPS;
+    var mPreviousTime;
+    var mLagTime;
+    var mCurrentTime;
+    var mIsLoopRunning = false;
+    var mMyGame = null;
+
+    var _runLoop = function(){
+        if(mIsLoopRunning){
+            requestAnimationFrame(function(){_runLoop.call(mMyGame);});
+            mCurrentTime = Date.now();
+            mElapsedTime = mCurrentTime - mPreviousTime;
+            mPreviousTime = mCurrentTime;
+            mLagTime += mElapsedTime;
+            while((mLagTime >= kMPF) && mIsLoopRunning){
+                this.update();
+                mLagTime -= kMPF;
+            }
+            this.draw();
+        }
+    };
+
+    var start = function(myGame){
+        mMyGame = myGame;
+        mPreviousTime = Date.now();
+        mLagTime = 0.0;
+        mIsLoopRunning = true;
+        requestAnimationFrame(function(){_runLoop.call(mMyGame);});
+    };
+    
+    var mPublic ={
+        start: start
+    };
+    return mPublic;
+}());
--- Organization/public_html/src/MyGame/MyGame.js
+++ Organization/public_html/src/MyGame/MyGame.js
@@ -5,37 +5,48 @@
  */
 
 function MyGame(htmlCanvasID){
+    this.mConstColorShader = null;
+    this.mWhiteSq = null;
+    this.mRedSq = null;
+    this.mCamera = null;
     gEngine.Core.initializeWebGL(htmlCanvasID);
+    this.initialize();
+}
+
+MyGame.prototype.initialize = function(){
     this.mCamera = new Camera(vec2.fromValues(20, 60), 20, [20, 40, 600, 300]);
+    this.mCamera.setBackgroundColor([0.8, 0.8, 0.8, 1]);
     this.mConstColorShader = new SimpleShader("src/GLSLShaders/SimpleVS.glsl", "src/GLSLShaders/SimpleFS.glsl");
-    this.mBlueSq = new Renderable(this.mConstColorShader);
-    this.mBlueSq.setColor([0.25,0.25,0.95,1]);
+    this.mWhiteSq = new Renderable(this.mConstColorShader);
+    this.mWhiteSq.setColor([1,1,1,1]);
     this.mRedSq = new Renderable(this.mConstColorShader);
-    this.mRedSq.setColor([1,0.25,0.25,1]);
-    this.mTLSq = new Renderable(this.mConstColorShader);
-    this.mTLSq.setColor([0.9,0.1,0.1,1]);
-    this.mTRSq = new Renderable(this.mConstColorShader);
-    this.mTRSq.setColor([0.1,0.9,0.1,1]);
-    this.mBRSq = new Renderable(this.mConstColorShader);
-    this.mBRSq.setColor([0.1,0.1,0.9,1]);
-    this.mBLSq = new Renderable(this.mConstColorShader);
-    this.mBLSq.setColor([0.1,0.1,0.1,1]);
-    gEngine.Core.clearCanvas([0.9, 0.9, 0.9, 1]);
-    this.mCamera.setupViewProjection();
-    var vpMatrix = this.mCamera.getVPMatrix();
-    this.mBlueSq.getXForm().setPosition(20, 60);
-    this.mBlueSq.getXForm().setRotationInRad(0.2);
-    this.mBlueSq.getXForm().setSize(5, 5);
-    this.mBlueSq.draw(vpMatrix);
+    this.mRedSq.setColor([1,0,0,1]);
+    this.mWhiteSq.getXForm().setPosition(20, 60);
+    this.mWhiteSq.getXForm().setRotationInRad(0.2);
+    this.mWhiteSq.getXForm().setSize(5,5);
     this.mRedSq.getXForm().setPosition(20, 60);
     this.mRedSq.getXForm().setSize(2, 2);
-    this.mRedSq.draw(vpMatrix);
-    this.mTLSq.getXForm().setPosition(10, 65);
-    this.mTLSq.draw(vpMatrix);
-    this.mTRSq.getXForm().setPosition(30, 65);
-    this.mTRSq.draw(vpMatrix);
-    this.mBRSq.getXForm().setPosition(30, 55);
-    this.mBRSq.draw(vpMatrix);
-    this.mBRSq.getXForm().setPosition(10, 55);
-    this.mBRSq.draw(vpMatrix);
-}
+    gEngine.GameLoop.start(this);
+};
+
+MyGame.prototype.update = function(){
+    var whiteXform = this.mWhiteSq.getXForm();
+    var deltaX = 0.05;
+    if(whiteXform.getXPos() > 30){
+        whiteXform.setPosition(10, 60);
+    }
+    whiteXform.incXPosBy(deltaX);
+    whiteXform.incRotationByDegree(1);
+    var redXform = this.mRedSq.getXForm();
+    if(redXform.getWidth() > 5){
+        redXform.setSize(2,2);
+    }
+    redXform.incSizeBy(0.05);
+};
+
+MyGame.prototype.draw = function(){
+    gEngine.Core.clearCanvas([0.9, 0.9, 0.9, 1]);
+    this.mCamera.setupViewProjection();
+    this.mWhiteSq.draw(this.mCamera.getVPMatrix());
+    this.mRedSq.draw(this.mCamera.getVPMatrix());
+};
